ddb500ff7c7576f43717cb33484527eab47eb927
Balint <balintmaci@gmail.com>
2023-11-28 15:50:43 +0100

    add basic tests for blacklist

---
+++ tests/blacklist_test.py
@@ -0,0 +1,26 @@
+from pytest import raises
+from fibonacci.blacklist import blacklist, unblacklist, get_blacklisted
+
+
+def test_empty_blacklist():
+    assert len(list(get_blacklisted())) == 0
+
+
+def test_bad_remove():
+    assert 1 not in get_blacklisted()
+    with raises(KeyError):
+        unblacklist(1)
+
+
+def test_blacklisting_multiple_numbers():
+    values = [3, 5, 9, 12, 34]
+    for v in values:
+        assert v not in get_blacklisted()
+    for v in values:
+        blacklist(v)
+    for v in values:
+        assert v in get_blacklisted()
+    for v in values:
+        unblacklist(v)
+    for v in values:
+        assert v not in get_blacklisted()
